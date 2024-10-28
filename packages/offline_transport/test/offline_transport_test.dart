5156a935d9cbf32688f756975ff333103715ab36
Robert Magnusson <robert@northmind.io>
2024-10-28 16:11:47 +0100

    Add placeholder test for offline_package

--- packages/offline_transport/test/offline_transport_test.dart
+++ packages/offline_transport/test/offline_transport_test.dart
@@ -1,1 +1,9 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('OfflineTransport:', () {
+    test('placeholder test', () {
+      expect(1, 1);
+    });
+  });
+}
