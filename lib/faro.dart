b4e826af633799fb74b9e900305959a3079abe59
Robert Magnusson <robert@northmind.io>
2025-07-02 14:59:01 +0200

    fix copilot feedback

--- lib/faro.dart
+++ lib/faro.dart
@@ -2,6 +2,7 @@ library;
 
 export './src/configurations/batch_config.dart';
 export './src/configurations/faro_config.dart';
+export './src/faro.dart';
 export './src/faro_asset_bundle.dart';
 export './src/faro_navigation_observer.dart';
 export './src/faro_user_interaction_widget.dart';
@@ -10,4 +11,3 @@ export './src/models/models.dart';
 export './src/offline_transport/offline_transport.dart';
 export './src/tracing/span.dart';
 export './src/transport/faro_transport.dart';
-export 'src/faro.dart';
