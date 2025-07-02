25a3fc8e70d5103b5b9d6a451d20ef12013b9c36
Robert Magnusson <robert@northmind.io>
2025-07-02 15:16:04 +0200

    chore: bump version to v0.4.0

--- lib/src/util/constants.dart
+++ lib/src/util/constants.dart
@@ -1,7 +1,7 @@
 /// Constants for the Faro Flutter SDK
 class FaroConstants {
   /// The version of the Faro Flutter SDK
-  static const String sdkVersion = '0.3.7';
+  static const String sdkVersion = '0.4.0';
 
   /// The name of the Faro Flutter SDK
   static const String sdkName = 'faro-flutter-sdk';
